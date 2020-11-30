6d0ad4e2828a7570994493698824a7b5ad9f8a28
violateer <1828257089@qq.com>
2020-11-30 16:22:02 +0800

    基础知识

--- index.ts
+++ index.ts
@@ -1,10 +1,20 @@
 import * as http from 'http';
+import { IncomingMessage, ServerResponse } from 'http';
 
 const server = http.createServer();
 
-server.on('request', (request, response) => {
-  console.log('有人请求了');
-  response.end('hi');
+server.on('request', (request: IncomingMessage, response: ServerResponse) => {
+  const array = [];
+  request.on('data', (chunk) => {
+    array.push(chunk);
+
+    request.on('end', () => {
+      const body = Buffer.concat(array).toString();
+      console.log(body);
+
+      response.end('hi');
+    });
+  });
 });
 
 server.listen('8888');
