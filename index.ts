556c5ec1f5b8fd37c4badeea619b35af5b890bae
violateer <1828257089@qq.com>
2020-11-30 17:11:47 +0800

    目标1：根据不同url返回不同文件

--- index.ts
+++ index.ts
@@ -1,20 +1,36 @@
 import * as http from 'http';
 import { IncomingMessage, ServerResponse } from 'http';
+import * as fs from 'fs';
+import * as p from 'path';
 
 const server = http.createServer();
+const publicDir = p.resolve(__dirname, 'public');
 
 server.on('request', (request: IncomingMessage, response: ServerResponse) => {
-  const array = [];
-  request.on('data', (chunk) => {
-    array.push(chunk);
-
-    request.on('end', () => {
-      const body = Buffer.concat(array).toString();
-      console.log(body);
-
-      response.end('hi');
-    });
-  });
+  const { method, url, headers } = request;
+  switch (url) {
+    case '/index.html':
+      response.setHeader('Content-Type', 'text/html;charset=utf-8');
+      fs.readFile(p.resolve(publicDir, 'index.html'), (err, data) => {
+        if (err) throw err;
+        response.end(data.toString());
+      });
+      break;
+    case '/style.css':
+      response.setHeader('Content-Type', 'text/css;charset=utf-8');
+      fs.readFile(p.resolve(publicDir, 'style.css'), (err, data) => {
+        if (err) throw err;
+        response.end(data.toString());
+      });
+      break;
+    case '/main.js':
+      response.setHeader('Content-Type', 'text/javascript;charset=utf-8');
+      fs.readFile(p.resolve(publicDir, 'main.js'), (err, data) => {
+        if (err) throw err;
+        response.end(data.toString());
+      });
+      break;
+  }
 });
 
 server.listen('8888');
